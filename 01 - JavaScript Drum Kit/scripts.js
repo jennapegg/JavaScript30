39084d743bcc54dfc9d39389c853f43f26cab2b8
Jenna Pegg <jenna.pegg.vic@gmail.com>
2019-01-21 13:00:35 -0800

    add js file, add keydown sound and transition end listeners

---
+++ 01 - JavaScript Drum Kit/scripts.js
@@ -0,0 +1,18 @@
+
+  function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if(!audio) return; //stops the function if no corresponding key code
+    audio.currentTime = 0; //rewinds the sound to the start
+    audio.play();
+    key.classList.add('playing');
+  };
+
+  function removeTransition(e) {
+    if(e.propertyName !== 'transform') return; // skip it if it's not a transform
+    this.classList.remove('playing');
+  }
+
+  const keys = document.querySelectorAll('.key');
+  window.addEventListener('keydown', playSound)
+  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
